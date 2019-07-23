eb63407a8cbc6b652bdac77a4dcfc4533e6a60fb
xu_long <xu_long@kingdee.com>
2019-07-23 16:08:30 +0800

    list样式调整

--- client/components/List.vue
+++ client/components/List.vue
@@ -2,21 +2,48 @@
  * @Author: xu.long 
  * @Date: 2019-07-20 15:45:25 
  * @Last Modified by: xu.long
- * @Last Modified time: 2019-07-20 16:00:39
+ * @Last Modified time: 2019-07-22 09:31:28
  */
 
 <template>
-    <div>
-        <slot />
-    </div>
+  <div class="list-container">
+    <slot />
+  </div>
 </template>
 <script>
-    export default {
-        name: '',
-        data(){
-            return {}
-        },
-        methods: function(){}
-    }
+export default {
+  name: "List",
+  data() {
+    return {};
+  },
+  methods: function() {}
+};
 </script>
-<style></style>
+<style lang="scss">
+.list-container {
+  background-color: #ffffff;
+  position: relative;
+  &::before {
+    content: "";
+    position: absolute;
+    z-index: 11;
+    width: 100%;
+    height: 1px;
+    top: 0;
+    left: 0;
+    background-color: rgb(230, 232, 238);
+    transform: scaleY(0.5);
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    z-index: 11;
+    width: 100%;
+    height: 1px;
+    bottom: 0;
+    left: 0;
+    background-color: rgb(230, 232, 238);
+    transform: scaleY(0.5);
+  }
+}
+</style>
